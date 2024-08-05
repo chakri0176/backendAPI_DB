c07e175809e33eb44f5f2d650ce804de9d48558d
mahesh jamdade <maheshmn121@gmail.com>
2024-08-04 20:42:23 -0400

    delete everything

--- zoo_backendAPI/zoo_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ZooAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'zoo_app'
--- zoo_backendAPI/zoo_app/mongo_client.py
+++
@@ -1,6 +0,0 @@
-# zoo_app/mongo_client.py
-from pymongo import MongoClient
-from django.conf import settings
-
-client = MongoClient(settings.MONGO_URI)
-db = client.get_database()
