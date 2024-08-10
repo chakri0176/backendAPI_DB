9e74b9196af2fdcfcec7443da1f3bac337a13948
mahesh jamdade <maheshmn121@gmail.com>
2024-08-09 21:31:31 -0400

    add animals route

--- backend/app/routes/animal_routes.py
+++ backend/app/routes/animal_routes.py
@@ -33,6 +33,70 @@ def get_animals():
 
     return jsonify(animals_list), 200
 
+@app.route('/add_animal', methods=['POST'])
+def add_animal():
+    data = request.json
+
+    # Insert into Species_info if not already present
+    species_query = {"Species_name": data["Species_name"]}
+    species = current_app.db["Species_info"].find_one(species_query)
+
+    if not species:
+        species_data = {
+            "Species_name": data["Species_name"],
+            "Species_type": data["Species_type"],
+            "Species_lifespan": data["Species_lifespan"],
+            "Food_type": data["Food_type"]
+        }
+        species_id = insert_document("Species_info", species_data)
+    else:
+        species_id = str(species["_id"])
+
+    # Insert into Enclosure_info if not already present
+    enclosure_query = {"Enclosure_name": data["Enclosure_name"]}
+    enclosure = current_app.db["Enclosure_info"].find_one(enclosure_query)
+
+    if not enclosure:
+        enclosure_data = {
+            "Enclosure_name": data["Enclosure_name"],
+            "Enclosure_type": data["Enclosure_type"],
+            "Enclosure_capacity": data["Enclosure_capacity"]
+        }
+        enclosure_id = insert_document("Enclosure_info", enclosure_data)
+    else:
+        enclosure_id = str(enclosure["_id"])
+
+    # Insert into Animal_info
+    animal_data = {
+        "Species_id": ObjectId(species_id),
+        "Animal_name": data["Animal_name"],
+        "Animal_sex": data["Animal_sex"],
+        "Animal_birthdate": datetime.strptime(data["Animal_birthdate"], "%Y-%m-%d"),
+        "Current_animal_location": ObjectId(enclosure_id)
+    }
+    animal_id = insert_document("Animal_info", animal_data)
+
+    # Insert into Animal_feeding
+    feeding_data = {
+        "Animal_id": ObjectId(animal_id),
+        "Feeding_action_type": data["Feeding_action_type"],
+        "Food_time": datetime.strptime(data["Food_time"], "%Y-%m-%dT%H:%M:%S.%f"),
+        "Food_type": data["Food_type"],
+        "Food_weight": data["Food_weight"]
+    }
+    insert_document("Animal_feeding", feeding_data)
+
+    # Insert into Animal_health
+    health_data = {
+        "Animal_id": ObjectId(animal_id),
+        "Health_event_type": data["Health_event_type"],
+        "Health_event_time": datetime.strptime(data["Health_event_time"], "%a, %d %b %Y %H:%M:%S GMT"),
+        "Health_event_comments": data["Health_event_comments"]
+    }
+    insert_document("Animal_health", health_data)
+
+    return jsonify({"message": "Animal and related records added successfully", "Animal_id": animal_id}), 201
+
 @app.route('/', methods=['POST'])
 def add_animal():
     new_animal = request.json
